7555ee6061d1f8d8a83ae0f2d7fc519adf2ea847
Ahmad <git@ahmadoqsrawi.com>
2026-04-10 08:57:50 +0200

    fix: remove missing AuthSuccessDetector import from detectors init

--- detectors/__init__.py
+++ detectors/__init__.py
@@ -1,7 +1,13 @@
-from .base          import BaseDetector
-from .brute_force   import BruteForceDetector
+
+from .base           import BaseDetector
+
+from .brute_force    import BruteForceDetector
+
 from .password_spray import PasswordSprayDetector
-from .enumeration   import EnumerationDetector
-from .priv_esc      import PrivEscDetector
-from .auth_success  import AuthSuccessDetector
-from .allowlist     import AllowlistEngine, DetectionPipeline
+
+from .enumeration    import EnumerationDetector
+
+from .priv_esc       import PrivEscDetector
+
+from .allowlist      import AllowlistEngine, DetectionPipeline
+
